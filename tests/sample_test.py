7e188e88bd7511ea98f6417e0e7d76a23599d420
KrezelewskiM <michalkrezelewski@gmail.com>
2016-10-27 22:58:34 +0200

    Add basic Flask test case

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -1,2 +1,10 @@
+import fast_paxos
+
+app = fast_paxos.app.test_client()
+
 def test_sample():
     assert 2 + 2 == 4
+
+def test_response():
+    response = app.get('/')
+    assert b'Hello World!' in response.data
