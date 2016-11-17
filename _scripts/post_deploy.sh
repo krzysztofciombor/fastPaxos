b0c3392856eeae9f620ec5d2e4a4525bab9b1108
KrezelewskiM <michalkrezelewski@gmail.com>
2016-11-17 11:54:44 +0100

    Remove unnecessary files

--- _scripts/post_deploy.sh
+++ _scripts/post_deploy.sh
@@ -5,8 +5,8 @@ pip3 install -r ../requirements.txt
 
 nohup ./start_coordinator.sh > /dev/null &
 
-for i in `seq 5001 5005`;
+for i in `seq 5001 5006`;
 do
-  nohup ./start_instance.sh $i 5 > /dev/null &
+  nohup ./start_instance.sh $i 6 > /dev/null &
 done
 exit 0
